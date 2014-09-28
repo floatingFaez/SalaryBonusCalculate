9abb45769891f6fa88ad14a58e29e1d7d84cd3ab
floatingFaez <floatingfaez@live.com>
2014-09-28 22:09:00 +0600

    Salary Bonus Project File initially uploaded

---
+++ SalaryBonusCalculate/SalaryBonusUI.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SalaryBonusCalculate
+{
+    partial class SalaryBonusForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputSalaryLabel = new System.Windows.Forms.Label();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputSalaryLabel
+            // 
+            this.inputSalaryLabel.AutoSize = true;
+            this.inputSalaryLabel.Location = new System.Drawing.Point(13, 27);
+            this.inputSalaryLabel.Name = "inputSalaryLabel";
+            this.inputSalaryLabel.Size = new System.Drawing.Size(121, 13);
+            this.inputSalaryLabel.TabIndex = 0;
+            this.inputSalaryLabel.Text = "Input Employee Salary : ";
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(141, 27);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(183, 20);
+            this.salaryTextBox.TabIndex = 1;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(330, 24);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(108, 23);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate Bonus";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // SalaryBonusForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 112);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.inputSalaryLabel);
+            this.Name = "SalaryBonusForm";
+            this.Text = "Employee Salary Bonus Calculate Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label inputSalaryLabel;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ SalaryBonusCalculate/SalaryBonusUI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SalaryBonusCalculate
+{
+    public partial class SalaryBonusForm : Form
+    {
+        public SalaryBonusForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double salaryBalance = Convert.ToDouble(salaryTextBox.Text);
+            double bonusPercen;
+
+            if (salaryBalance > 10000)
+                bonusPercen = salaryBalance * .05;
+            else if (salaryBalance == 10000)
+                bonusPercen = salaryBalance * .06;
+            else
+                bonusPercen = salaryBalance * .07;
+
+            MessageBox.Show("Employee Bonus is : " + bonusPercen);
+        }
+    }
+}
